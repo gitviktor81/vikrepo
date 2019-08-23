4166080a72b7e7a177d2a3f829e4c0ad8e814477
Kiss Viktor <viktor.kiss@dorsum.eu>
2019-08-23 08:21:27 +0200

    adding base projects

---
+++ ElsoJava/src/main/java/com/example/ElsoJava/ElsoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ElsoJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsoJavaApplication.class, args);
+	}
+
+}
