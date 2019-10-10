32af93ba46ff27d23cd6f1e35da27701cff0ec68
Kiss Viktor <viktor.kiss@dorsum.eu>
2019-10-10 08:05:10 +0200

    Basic auth by AuthenticationManagerBuilder - Secured annotation on controller method

--- Sec/src/main/java/com/sec/config/SecurityConf.java
+++ Sec/src/main/java/com/sec/config/SecurityConf.java
@@ -1,11 +1,28 @@
 package com.sec.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 // Meg tudjuk vele mondani hogy melyik controllerben definiált fgvünket milyen szintű felhasználó érhesse el
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
 public class SecurityConf extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    protected void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("kiv")
+                .password("{noop}letmein")
+                .roles("USER")
+                .and()
+                .withUser("kivadmin")
+                .password("{noop}letmein")
+                .roles("ADMIN");
+    }
 }
